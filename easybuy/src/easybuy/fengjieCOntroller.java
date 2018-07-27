ebaedd4c6ef4e60768df961790e5043fe10fddab
RALJ7JY02N1P36G\Administrator <846961836@qq.com>
2018-07-27 11:56:57 +0800

    liuyancommit

---
+++ easybuy/src/easybuy/fengjieCOntroller.java
@@ -0,0 +1,10 @@
+package easybuy;
+
+import java.net.Socket;
+
+public class fengjieCOntroller {
+
+	public static void main(String[] args) {
+		System.out.println("fengjie");
+	}
+}
---
+++ easybuy/src/easybuy/liuyanController.java
@@ -0,0 +1,8 @@
+package easybuy;
+
+public class liuyanController {
+
+	public static void main(String[] args) {
+		System.out.println("liuyancontroller");
+	}
+}
